8bdae11a82ef6f8dbb25f06f4a8549689acfbb62
shenjianwei <shenjianwei@xindong.com>
2016-03-21 19:56:28 +0800

    levels

--- LightDarkness/src/ui/LevelsUI.js
+++ LightDarkness/src/ui/LevelsUI.js
@@ -7,6 +7,6 @@ var LevelsUI=cc.Layer.extend({
         var bg=new cc.Sprite(res.selectBg);
         bg.setPosition(cc.p(cc.director.getWinSize().width/2,cc.director.getWinSize().height/2));
         this.addChild(bg);
-        
+
     }
 });
